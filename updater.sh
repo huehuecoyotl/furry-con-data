8c6ca5e23453d5a1c57ccc2fd022f5cf15107f1d
Huehuecóyotl <41935818+huehuecoyotl@users.noreply.github.com>
2018-07-31 13:22:34 +0000

    give up on that script son

--- updater.sh
+++ updater.sh
@@ -1,8 +1,7 @@
+#!/bin/bash
+
 cd /home/ubuntu/data_muncher
 
-if git diff --quiet
-  then
-    git pull
-    ruby data_processor.rb
-    cp ./processed_data/* /home/ubuntu/coyotl/source/public/data/
-fi
+git pull
+ruby data_processor.rb
+cp ./processed_data/* /home/ubuntu/coyotl/source/public/data/
