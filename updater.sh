7c8fba9a18b5f186531e3889975613f06f5c33f3
Huehuecóyotl <41935818+huehuecoyotl@users.noreply.github.com>
2018-07-31 08:09:52 -0500

    fix updater

--- updater.sh
+++ updater.sh
@@ -1,7 +1,7 @@
 cd /home/ubuntu/data_muncher
 
-if [ git diff --quiet ]
-then
+if git diff --quiet
+  then
     git pull
     ruby data_processor.rb
     cp ./processed_data/* /home/ubuntu/coyotl/source/public/data/
