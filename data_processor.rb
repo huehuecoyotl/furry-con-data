186a03c3bd9a139d172bc1736e964ad6617388d5
Huehuecóyotl <41935818+huehuecoyotl@users.noreply.github.com>
2020-12-19 07:28:22 -0600

    new way to calculate it

--- data_processor.rb
+++ data_processor.rb
@@ -11,68 +11,137 @@ require 'date'
 require 'json'
 
 class String
-    def is_i?
-       /\A[-+]?\d+\z/ === self
+    def is_date_ish?
+      /\A\d{1,2}\/\d{1,2}\/\d{4}\z/ === self
     end
 end
 
+class SortaDate
+  include Comparable
+
+  def initialize(year=0, day=0)
+    if year.is_a?(String)
+      date = Date.strptime(year, "%m/%d/%Y")
+      @year = Integer(date.strftime("%Y"))
+      @day = Integer(date.strftime("%j"), 10) - 1
+    else
+      @year = year
+      @day = day
+    end
+  end
+
+  def is_leap_year?
+    Date.gregorian_leap?(@year)
+  end
+
+  def to_f
+    @year + (@day.to_f / (self.is_leap_year? ? 366 : 365))
+  end
+
+  def to_i
+    @year
+  end
+
+  def <=>(otherDate)
+    retval = @year <=> otherDate.year
+    if retval == 0
+      @day <=> otherDate.day
+    else
+      retval
+    end
+  end
+
+  def +(time)
+    days = (time * 365).to_i
+
+    retval = SortaDate.new(@year, @day)
+    retval.day += days
+    
+    while retval.day >= (retval.is_leap_year? ? 366 : 365)
+      retval.day -= (retval.is_leap_year? ? 366 : 365)
+      retval.year += 1
+    end
+
+    retval
+  end
+
+  def -(time)
+    if time.is_a?(SortaDate)
+      days = (@year - time.year) * 365
+      days += @day - time.day
+      days.to_f / 365
+    else
+      days = (time * 365).to_i
+
+      retval = SortaDate.new(@year, @day)
+      retval.day -= days
+      
+      while retval.day < 0
+        retval.year -= 1
+        retval.day += (retval.is_leap_year? ? 366 : 365)
+      end
+
+      retval
+    end
+  end
+
+  attr_accessor :year
+  attr_accessor :day
+end
+
 class ConYear
-  def initialize
-    @date = "N/A"
-    @attendance = 0
-    @prevDate = "N/A"
-    @twelveMonthsAttendance = 0
+  def initialize(attendance=0, dateYear=0, dateInt=0)
+    @date = SortaDate.new(dateYear, dateInt)
+    @prevDate = @date - 1.025
+    @attendance = Integer(attendance)
+    @prevCons = Array.new
   end
 
-  def set_data(date, attendance)
-    @date = date == "*" ? "*" : Date.strptime(date, "%m/%d/%Y")
-    @attendance = attendance == "*" ? "*" : Integer(attendance)
+  def set_prev_date(dateYear=0, dateInt=0)
+    if dateYear.is_a?(SortaDate)
+      newDate = dateYear
+    else
+      newDate = SortaDate.new(dateYear, dateInt)
+    end
+    
+    if newDate > @prevDate
+      @prevDate = newDate
+      @prevCons.select! {|x| x.date > @prevDate and x.date <= @date}
+    end
   end
 
-  def set_prev_date(prevDate)
-    @prevDate = prevDate
+  def add_to_previous_cons(newConYear)
+    @prevCons << newConYear if newConYear.date > @prevDate and newConYear.date <= @date
   end
 
-  def add_to_twelve_months(newAttendance)
-    @twelveMonthsAttendance += newAttendance
+  def get_market_share_comparison
+    @prevCons.inject(0){ |sum, x| sum + x.attendance }
   end
 
   attr_reader :date
   attr_reader :attendance
   attr_reader :prevDate
-  attr_reader :twelveMonthsAttendance
+  attr_reader :prevCons
 end
 
-# pr
 def prepare_data(allCSVs)
-  formattedData = Hash.new { |hash, key| hash[key] = Hash.new { |h, k| h[k] = ConYear.new } }
-  actualOutput = Hash.new { |hash, key| hash[key] = Array.new() }
-  minYear = 3000
-  maxYear = 0
+  formattedData = Hash.new { |hash, key| hash[key] = Array.new }
+  actualOutput = Hash.new { |hash, key| hash[key] = Array.new }
+  minYear = Float::INFINITY
+  maxYear = -Float::INFINITY 
 
   allCSVs.each do |currCon|
     conName = currCon[0][0]
     
-    actualOutput[conName + '-date'] << (conName + '-date')
-    actualOutput[conName + '-attendance'] << conName
-    
     currCon.each do |currYear|
-      next unless currYear[0].is_i?
+      next unless currYear[1] and currYear[1].is_date_ish?
 
-      unless currYear[1] == "*" || currYear[2] == "*"
-        currDate = Date.strptime(currYear[1], "%m/%d/%Y")
-        currDateInt = Integer(currDate.strftime("%j"), 10) - 1
-        currDateYear = Integer(currDate.strftime("%Y"))
+      currConYear = ConYear.new(currYear[2], currYear[1])
 
-        minYear = (currDateYear < minYear ? currDateYear : minYear)
-        maxYear = (currDateYear > maxYear ? currDateYear : maxYear)
-        currDateInt = Date.gregorian_leap?(currDateYear) ? currDateYear + currDateInt.to_f / 366 : currDateYear + currDateInt.to_f / 365
+      minYear = (currConYear.date.to_i < minYear ? currConYear.date.to_i : minYear)
+      maxYear = (currConYear.date.to_i > maxYear ? currConYear.date.to_i : maxYear)
 
-        actualOutput[conName + '-date'] << currDateInt
-        actualOutput[conName + '-attendance'] << Integer(currYear[2])
-      end
-
-      formattedData[conName][Integer(currYear[0])].set_data(currYear[1], currYear[2])
+      formattedData[conName] << currConYear
     end
   end
 
@@ -80,117 +149,55 @@ def prepare_data(allCSVs)
   actualOutput['minYear'] = minYear
   actualOutput['maxYear'] = maxYear
 
+  maxAttendances = get_max_attendances formattedData
+  sortedConNames = (maxAttendances.sort_by { |k, v| -v }).map { |n| n[0] }
+  actualOutput["sortOrder"] = sortedConNames
+
   formattedData.each do |conName, conYears|
-    conYears.to_a.each_with_index do |arr, i|
-      if i == 0
-        prevDate = arr[1].date
-        prevDate = prevDate - (Date.gregorian_leap?(arr[0]) ? 366 : 365)
-        formattedData[conName][arr[0]].set_prev_date prevDate
-      else
-        prevDate = formattedData[conName][arr[0] - 1].date
-        if prevDate == "*" || prevDate == "N/A"
-          prevDate = arr[1].date
-          prevDate = prevDate - (Date.gregorian_leap?(arr[0]) ? 366 : 365)
-        end
-        formattedData[conName][arr[0]].set_prev_date prevDate
-      end
+    conYears.each_with_index do |currYear, i|
+      currYear.set_prev_date formattedData[conName][i - 1].date unless i == 0
     end
   end
 
   formattedData.each do |conName, conYears|
-    conYears.to_a.each_with_index do |arr, i|
-      if i == 0
-        prevDate = arr[1].date
-        prevDate = prevDate - (Date.gregorian_leap?(arr[0]) ? 366 : 365)
-        formattedData[conName][arr[0]].set_prev_date prevDate
-      else
-        prevDate = formattedData[conName][arr[0] - 1].date
-        if prevDate == "*" || prevDate == "N/A"
-          prevDate = arr[1].date
-          prevDate = prevDate - (Date.gregorian_leap?(arr[0]) ? 366 : 365)
-        end
-        formattedData[conName][arr[0]].set_prev_date prevDate
-      end
-    end
-  end
-
-  formattedData.each do |currConName, currConYears|
-    currConYears.each do |currYear, currSpecificData|
+    conYears.each do |currYear|
       formattedData.each do |otherConName, otherConYears|
-        next if currConName == otherConName
-        next if currSpecificData.date == "*" || currSpecificData.date == "N/A" || currSpecificData.attendance == "*" || currSpecificData.attendance == 0
-
-        competingAttendance = 0
-
-        otherSpecificData = formattedData[otherConName][currYear]
-        unless otherSpecificData.date == "*" || otherSpecificData.date == "N/A" || otherSpecificData.attendance == "*" || otherSpecificData.attendance == 0
-          if otherSpecificData.date < currSpecificData.date && otherSpecificData.date > currSpecificData.prevDate
-            competingAttendance += otherSpecificData.attendance 
-          end
-        end
-        
-        otherSpecificData = formattedData[otherConName][currYear - 1]
-        unless otherSpecificData.date == "*" || otherSpecificData.date == "N/A" || otherSpecificData.attendance == "*" || otherSpecificData.attendance == 0
-          if otherSpecificData.date <= currSpecificData.date && otherSpecificData.date > currSpecificData.prevDate
-            competingAttendance += otherSpecificData.attendance 
-          end    
-        end
-
-        formattedData[currConName][currYear].add_to_twelve_months competingAttendance
+        otherConYears.each{ |x| currYear.add_to_previous_cons x }
       end
     end
   end
 
-  return formattedData, actualOutput
+  formattedData.each do |conName, conYears|
+    actualOutput[conName + '-date'] << (conName + '-date')
+    actualOutput[conName + '-attendance'] << conName
+    actualOutput[conName + '-twelveMonths'] << conName
+    
+    conYears.each_with_index do |currYear, i|
+      if i > 0 and currYear.date - formattedData[conName][i - 1].date > 1.5
+        medianTime = (currYear.date - formattedData[conName][i - 1].date) / 2
+        actualOutput[conName + '-date'] << (currYear.date - medianTime).to_f
+        actualOutput[conName + '-attendance'] << "*"
+        actualOutput[conName + '-twelveMonths'] << "*"
+      end
+      actualOutput[conName + '-date'] << currYear.date.to_f
+      actualOutput[conName + '-attendance'] << currYear.attendance
+      actualOutput[conName + '-twelveMonths'] << (currYear.attendance.to_f / currYear.get_market_share_comparison)
+    end
+  end
+
+  actualOutput
 end
 
 def get_max_attendances(formattedData)
-  maxAttendances = Hash.new { |hash, key| hash[key] = 0 }
+  maxAttendances = Hash.new { |hash, key| hash[key] = -Float::INFINITY }
 
   formattedData.each do |conName, conYears|
-    conYears.each do |year, specificData|
-      next if specificData.date == "*" || specificData.date == "N/A" || specificData.attendance == "*" || specificData.attendance == 0
-
-      maxAttendances[conName] = specificData.attendance if specificData.attendance > maxAttendances[conName]
+    conYears.each do |currYear|
+      maxAttendances[conName] = currYear.attendance if currYear.attendance > maxAttendances[conName]
     end
   end
 
   maxAttendances
-end
-
-def generate_calendar_year_data(formattedData, actualOutput)
-  totalsForYear = Hash.new { |hash, key| hash[key] = 0.0 }
-  formattedData.each do |conName, conYears|
-    conYears.each do |year, specificData|
-      next if specificData.date == "*" || specificData.date == "N/A" || specificData.attendance == "*" || specificData.attendance == 0
-
-      totalsForYear[year] += specificData.attendance
-    end
-  end
-
-  formattedData.each do |conName, conYears|
-    actualOutput[conName + '-calYear'] << conName
-    conYears.each do |year, specificData|
-      next if specificData.date == "*" || specificData.date == "N/A" || specificData.attendance == "*" || specificData.attendance == 0
-
-      actualOutput[conName + '-calYear'] << specificData.attendance / totalsForYear[year]
-    end
-  end
-
-  actualOutput
-end
-
-def generate_twelve_months_data(formattedData, actualOutput)
-  formattedData.each do |conName, conYears|
-    actualOutput[conName + '-twelveMonths'] << conName
-    conYears.each do |year, specificData|
-      next if specificData.date == "*" || specificData.date == "N/A" || specificData.attendance == "*" || specificData.attendance == 0
-
-      actualOutput[conName + '-twelveMonths'] << specificData.attendance.to_f / (specificData.twelveMonthsAttendance + specificData.attendance)
-    end
-  end
-
-  actualOutput
 end
 
 allCSVs = Array.new
@@ -200,14 +207,7 @@ Dir.foreach('raw_data') do |filename|
   allCSVs << CSV.read("raw_data/" + filename)
 end
 
-formattedData, actualOutput = prepare_data allCSVs
-
-maxAttendances = get_max_attendances formattedData
-sortedConNames = (maxAttendances.sort_by { |k, v| -v }).map { |n| n[0] }
-actualOutput["sortOrder"] = sortedConNames
-
-actualOutput = generate_calendar_year_data(formattedData, actualOutput)
-actualOutput = generate_twelve_months_data(formattedData, actualOutput)
+actualOutput = prepare_data allCSVs
 
 File.open("viz_data.json", "w") do |fout|
    fout.syswrite JSON.pretty_generate(actualOutput)
