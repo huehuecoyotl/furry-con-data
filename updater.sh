9aebf171ac1d741dfa9ec58f58f1de665c1c4680
Huehuecóyotl <41935818+huehuecoyotl@users.noreply.github.com>
2023-06-21 02:10:36 -0500

    force refresh of update tracking when new changes pushed

--- updater.sh
+++ updater.sh
@@ -7,6 +7,7 @@ if !(git status -uno | grep -q "Your branch is up to date with 'origin/master'."
 then
     git pull
     ./data_processor.rb
+    /home/ubuntu/log_alert/updater.sh
 fi
 mkdir -p /home/ubuntu/coyotl/source/public/data/
 cp --preserve ./viz_data.json /home/ubuntu/coyotl/source/public/data/
